b6df9f85de6c2a6aa088cca243815020ed843ca8
Martin Bridgman <martin.bridgman@gmail.com>
2021-10-27 22:23:39 +0000

    Add increment runs on scoreboard script to playBall function

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    gameStart();
+});
 // Event listener - game start button
 // calls gameStart function
 let startGame = document.getElementById("start-game");
@@ -35,7 +38,7 @@ function generateScore(){
 }
 
 /**
- * playBall function: generates random number and compares random number against balls array.
+ * playBall function: generates random number and compares random number against batDie array.
  * if runs scored - increments runs of on-strike batsman, updates scoreboards and scorecards and displays a message to user.
  * if runs %2 !0 switches on-strike (class) batsman to non-striker (class) and vice versa.
  * if Owzthat - displays Owzthat message to user.
@@ -43,10 +46,87 @@ function generateScore(){
  * If runs > target game ends and end game function called.                                                                                                                                                                                                
  */
 function playBall(){
-    let batDie = [0, 0, 1, 1, 1, 2, 3, 4, 6, "Owzthat"];
-    let ball = Math.floor(Math.random() * 10);
+    let batDie = ["0a", "0b", "1a", "1b", "1c", "2a", "2b", 3, 4, 6, "Owzthat"];
+    let ball = Math.floor(Math.random() * 11);    
     let runs = batDie[ball];
+    
+    let batRuns = parseInt(document.getElementsByClassName('on-strike')[0].innerText); 
+    let dialogueBox = document.getElementById("dialogue-box");
+    let batsmanUpdate = document.getElementsByClassName("on-strike")[0];
+    let incrementRuns;
+
+    switch (runs){
+        case "0a":            
+            dialogueBox.textContent = "Well defended but no runs";
+        break;
+    
+        case "0b":            
+            dialogueBox.textContent ="shot and miss, no runs";
+        break;
+    
+        case "1a":            
+            dialogueBox.textContent = "Well done, You've sneaked it through the infield for one run";
+            incrementRuns = batRuns + 1;
+            batsmanUpdate.textContent = incrementRuns;
+        break;
+    
+        case "1b":
+            
+            dialogueBox.textContent = "quick single, well run";
+            incrementRuns = batRuns +1;
+            batsmanUpdate.textContent = incrementRuns;
+        break;
+    
+        case "1c":
+            
+            dialogueBox.textContent = "Nice shot for a comfortable single";
+            incrementRuns = batRuns + 1;
+            batsmanUpdate.textContent = incrementRuns;
+        break;
+    
+        case "2a":
+            
+            dialogueBox.textContent = "good shot, two runs taken";
+            incrementRuns = batRuns + 2;
+            batsmanUpdate.textContent = incrementRuns;
+        break;
+
+        case "2b":
+            
+            dialogueBox.textContent = "guided into the outfield for an easy two runs";
+            incrementRuns = batRuns + 2;
+            batsmanUpdate.textContent = incrementRuns;
+        break;
+    
+        case 3:
+            
+            dialogueBox.textContent = "Nice hit but fielded in the deep, Well run for three";
+            incrementRuns = batRuns + 3;
+            batsmanUpdate.textContent = incrementRuns;
+        break;
+    
+        case 4:
+            
+            dialogueBox.textContent = "Great shot to the boundary, 4 runs";
+            incrementRuns = batRuns + 4;
+            batsmanUpdate.textContent = incrementRuns;
+        break;
+    
+        case 6:
+            
+            dialogueBox.textContent = "That has gone like a rocket and cleared the boundary ropes by a mile, Max 6 runs";
+            incrementRuns = batRuns + 6;
+            batsmanUpdate.textContent = incrementRuns;
+        break;
+    
+        case "Owzthat":
+            dialogueBox.textContent = "OWZ-that!!! - The fielding side are appealing. Roll the umpire die to learn your fate";
+        break;
+    
+    }
 }
+
+
 
 /**
  * umpire function: generates random number and compares random number with umpire array. 
