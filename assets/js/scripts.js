6c36dfd7e2271b811bc798182958e74907377f01
Martin Bridgman <martin.bridgman@gmail.com>
2021-11-09 21:08:51 +0000

    Remove commenting out of play ball and Owzthat disabled

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -95,7 +95,7 @@ function gameStart(){
     let modal = document.getElementById("start-modal");
     modal.style.display="block";
     resetScoreboard();
-    //document.getElementById("play-Owzthat").disabled = true;
+    document.getElementById("play-Owzthat").disabled = true;
     
     let submitForm = document.getElementById("start-dialogue-submit");
     submitForm.addEventListener("click", function(event){
@@ -204,70 +204,70 @@ function playBall(){
     let ball = Math.floor(Math.random() * 11);    
     let runs = batDie[ball];
     let dialogueBox = document.getElementById("dialogue-box");
-   /*  dialogueBox.textContent = "The bowler is running in ... ";
+   dialogueBox.textContent = "The bowler is running in ... ";
     setTimeout (function(){dialogueBox.textContent = " ... He delivers the ball";}, 1000);
-    setTimeout(function(){ */
-    switch (runs){
-        case "0a":
-            dialogueBox.textContent = "Well defended but no runs";
-        break;
-    
-        case "0b":            
-            dialogueBox.textContent ="shot and miss, no runs";
-        break;
-    
-        case "1a":            
-            dialogueBox.textContent = "Well done, You've sneaked it through the infield for one run";
-            incrementRuns(1);
-            toggle();            
-        break;
-    
-        case "1b":            
-            dialogueBox.textContent = "quick single, well run";
-            incrementRuns(1);
-            toggle();
-        break;
-    
-        case "1c":            
-            dialogueBox.textContent = "Nice shot for a comfortable single";
-            incrementRuns(1);
-            toggle();
-        break;
-    
-        case "2a":            
-            dialogueBox.textContent = "good shot, two runs taken";
-            incrementRuns(2);
-        break;
+    setTimeout(function(){ 
+        switch (runs){
+            case "0a":
+                dialogueBox.textContent = "Well defended but no runs";
+            break;
+        
+            case "0b":            
+                dialogueBox.textContent ="shot and miss, no runs";
+            break;
+        
+            case "1a":            
+                dialogueBox.textContent = "Well done, You've sneaked it through the infield for one run";
+                incrementRuns(1);
+                toggle();            
+            break;
+        
+            case "1b":            
+                dialogueBox.textContent = "quick single, well run";
+                incrementRuns(1);
+                toggle();
+            break;
+        
+            case "1c":            
+                dialogueBox.textContent = "Nice shot for a comfortable single";
+                incrementRuns(1);
+                toggle();
+            break;
+        
+            case "2a":            
+                dialogueBox.textContent = "good shot, two runs taken";
+                incrementRuns(2);
+            break;
 
-        case "2b":
-            dialogueBox.textContent = "guided into the outfield for an easy two runs";
-            incrementRuns(2);
-        break;
-    
-        case 3:
-            dialogueBox.textContent = "Nice hit but fielded in the deep, Well run for three";
-            incrementRuns(3);
-            toggle();
-        break;
-    
-        case 4:            
-            dialogueBox.textContent = "Great shot to the boundary, 4 runs";
-            incrementRuns(4);
-        break;
-    
-        case 6:            
-            dialogueBox.textContent = "That has gone like a rocket and cleared the boundary ropes by a mile, Max 6 runs";
-            incrementRuns(6);
-        break;
-    
-        case "Owzthat":
-            dialogueBox.textContent = "OWZ-that!!! - The fielding side are appealing. Click on the OWZthat button to learn your fate";
-            // document.getElementById("play-Owzthat").disabled = false;
-            //  document.getElementById("play-ball").disabled = true; 
-        break;  
-    }
-}/* , 2500);
-} */
+            case "2b":
+                dialogueBox.textContent = "guided into the outfield for an easy two runs";
+                incrementRuns(2);
+            break;
+        
+            case 3:
+                dialogueBox.textContent = "Nice hit but fielded in the deep, Well run for three";
+                incrementRuns(3);
+                toggle();
+            break;
+        
+            case 4:            
+                dialogueBox.textContent = "Great shot to the boundary, 4 runs";
+                incrementRuns(4);
+            break;
+        
+            case 6:            
+                dialogueBox.textContent = "That has gone like a rocket and cleared the boundary ropes by a mile, Max 6 runs";
+                incrementRuns(6);
+            break;
+        
+            case "Owzthat":
+                dialogueBox.textContent = "OWZ-that!!! - The fielding side are appealing. Click on the OWZthat button to learn your fate";
+                document.getElementById("play-Owzthat").disabled = false;
+                document.getElementById("play-ball").disabled = true; 
+            break;  
+        }
+    } , 2500);
+}
 
 /**
  * incrementRuns function: 
@@ -353,8 +353,8 @@ function umpire(){
         }
     }, 2000);
 
-    //document.getElementById("play-Owzthat").disabled = true;
-    //document.getElementById("play-ball").disabled = false;
+    document.getElementById("play-Owzthat").disabled = true;
+    document.getElementById("play-ball").disabled = false;
 }
 
 /**
